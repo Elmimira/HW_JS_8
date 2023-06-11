5aeb6214a1a12f12a18a63655be39b08fef6487b
Elmimira <elmira.volohova@gmail.com>
2023-06-12 00:54:33 +0300

    Use of functions

--- main.js
+++ main.js
@@ -1,46 +1,66 @@
 // Массив объектов с продуктами и ценами
 var products = [
-    { название: "Яблоко", цена: 10 },
-    { название: "Груша", цена: 15 },
-    { название: "Банан", цена: 8 },
-    { название: "Апельсин", цена: 12 },
+{ название: "Яблоко", цена: 10 },
+{ название: "Груша", цена: 15 },
+{ название: "Банан", цена: 8 },
+{ название: "Апельсин", цена: 12 },
 ];
 
-  // Вывод списка с данными по товарам в консоль
-console.log("Список товаров:");
+// Функция вывода списка продуктов в консоль
+function printProductList() {
 for (var i = 0; i < products.length; i++) {
-    console.log(i + 1 + ". " + products[i].название + " - " + products[i].цена + " грн");
+    var product = products[i];
+    console.log(
+    i + 1 + ". " + product.название + " - " + product.цена + " грн"
+    );
+}
 }
 
-  // Получение номера товара от пользователя
-var userInput = prompt("Введите номер товара:");
-var productIndex = parseInt(userInput) - 1;
-
-  // Проверка правильности ввода номера товара
-while (isNaN(productIndex) || productIndex < 0 || productIndex >= products.length) {
-    userInput = prompt("Некорректный номер товара. Попробуйте еще раз:");
-    productIndex = parseInt(userInput) - 1;
+// Функция проверки правильности ввода номера товара
+function validateProductNumber(number) {
+return !isNaN(number) && number > 0 && number <= products.length;
 }
 
-  // Получение количества товара от пользователя
-userInput = prompt("Введите количество товара:");
-var quantity = parseInt(userInput);
-
-  // Проверка правильности ввода количества товара
-while (isNaN(quantity) || quantity <= 0) {
-    userInput = prompt("Некорректное количество товара. Попробуйте еще раз:");
-    quantity = parseInt(userInput);
+// Функция проверки правильности ввода количества товара
+function validateQuantity(quantity) {
+return !isNaN(quantity) && quantity > 0;
 }
 
-  // Вычисление стоимости покупки
-  var totalPrice = products[productIndex].цена * quantity;
+// Функция расчета итоговой стоимости покупки
+function calculateTotalPrice(productIndex, quantity) {
+var product = products[productIndex - 1];
+  var totalPrice = product.цена * quantity;
 
-  // Проверка наличия скидки
 if (totalPrice > 10000) {
-    var discount = totalPrice * 0.2;
-    var discountedPrice = totalPrice - discount;
-    console.log("Итоговая стоимость с учетом скидки: " + discountedPrice + " грн");
-    console.log("Вам предоставлена скидка в размере " + discount + " грн");
-} else {
-    console.log("Итоговая стоимость: " + totalPrice + " грн");
+    totalPrice *= 0.8; // Применяем скидку 20%
+    console.log("Ваша покупка превышает 10 000 грн. Скидка 20% применена.");
 }
+
+console.log("Итоговая стоимость покупки: " + totalPrice + " грн");
+}
+
+// Выводим список продуктов в консоль
+printProductList();
+
+// Получаем номер товара от пользователя
+var productNumber = parseInt(prompt("Введите номер товара:"));
+
+// Проверяем правильность ввода номера товара
+while (!validateProductNumber(productNumber)) {
+productNumber = parseInt(
+    prompt("Неверный номер товара. Введите номер товара:")
+);
+}
+
+// Получаем количество товара от пользователя
+var quantity = parseInt(prompt("Введите количество товара:"));
+
+// Проверяем правильность ввода количества товара
+while (!validateQuantity(quantity)) {
+quantity = parseInt(
+    prompt("Неверное количество товара. Введите количество товара:")
+);
+}
+
+// Рассчитываем и выводим итоговую стоимость покупки
+calculateTotalPrice(productNumber, quantity);
